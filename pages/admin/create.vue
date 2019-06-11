543fb4b56c42278a257c9ffa119724ed69fe1092
prewe <45303311+prewe@users.noreply.github.com>
2019-06-11 14:24:08 +0700

    finish front-end

--- pages/admin/create.vue
+++ pages/admin/create.vue
@@ -1,50 +1,57 @@
 <template>
-  <el-form 
-    :model="controls" 
-    :rules="rules" 
-    ref="form" 
-    @submit.native.prevent="onSubmit"
-  >
-
+  <el-form :model="controls" :rules="rules" ref="form" @submit.native.prevent="onSubmit">
     <h1 class="mb">Создать новый пост</h1>
 
     <el-form-item label="Введите название поста" prop="title">
-      <el-input 
-        v-model.trim="controls.text"
-      />
+      <el-input v-model.trim="controls.title"/>
     </el-form-item>
 
     <el-form-item label="Текст в формате .md или .html" prop="text">
-      <el-input 
-        v-model.trim="controls.text"
-        type="textarea"
-        resize="none"
-        :rows="10"
-      />
+      <el-input v-model="controls.text" type="textarea" resize="none" :rows="10"/>
     </el-form-item>
-    
+
+    <el-button class="mb" type="success" plain @click="previewDialog = true">Предпросмотр</el-button>
+
+    <el-dialog title="Предпросмотр" :visible.sync="previewDialog">
+      <div :key="controls.text">
+        <vue-markdown>{{controls.text}}</vue-markdown>
+      </div>
+    </el-dialog>
+
+    <el-upload
+      class="mb"
+      drag
+      ref="upload"
+      action="https://jsonplaceholder.typicode.com/posts/"
+      :on-change="handleImageChange"
+      :auto-upload="false"
+    >
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">
+        Перетащите картинку
+        <em>или нажмите</em>
+      </div>
+      <div class="el-upload__tip" slot="tip">файлы с расширением jpg/png</div>
+    </el-upload>
+
     <el-form-item>
-      <el-button 
-        type="primary" 
-        round native-type="submit" 
-        :loading="loading"
-      >
-        Создать пост
-      </el-button>
+      <el-button type="primary" round native-type="submit" :loading="loading">Создать пост</el-button>
     </el-form-item>
   </el-form>
 </template>
 
 <script>
 export default {
-  layout: 'admin',
-  middleware: ['admin-auth'],
+  layout: "admin",
+  middleware: ["admin-auth"],
   data() {
     return {
+      image: null,
+      previewDialog: false,
       loading: false,
       controls: {
-        title: '',
-        text: ''
+        title: "",
+        text: ""
       },
       rules: {
         title: [
@@ -65,37 +72,43 @@ export default {
     };
   },
   methods: {
+    handleImageChange(file, fileList) {
+      this.image = file.raw
+    },
     onSubmit() {
       this.$refs.form.validate(async valid => {
-        if (valid) {
-          this.loading = true
+        if (valid && this.image) {
+          this.loading = true;
 
           const formData = {
             title: this.controls.title,
-            text: this.controls.text
-          }
+            text: this.controls.text,
+            image: this.image
+          };
 
           try {
-            await this.$store.dispatch('post/create', formData) // Я остановился на создании create 12 видео 8 минута
-            this.controls.text = ''
-            this.controls.title = ''
-            this.$message.success('Пост создан')
-            this.loading = false
+            await this.$store.dispatch("post/create", formData); // Я остановился на создании create 12 видео 8 минута
+            this.controls.text = ""
+            this.controls.title = ""
+            this.image = null
+            this.$refs.upload.clearFiles()
+            this.$message.success("Пост создан")
           } catch (e) {
+          } finally {
             this.loading = false
           }
+        } else {
+          this.$message.warning("Форма не валидна")
         }
-      })
+      });
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-
-  form {
-    max-width: 600px;
-  }
-
+form {
+  max-width: 600px;
+}
 </style>
 
